cca39fba59d0f397e2995fb81f67b8e35a9678c3
Hastings Greer <hastings.greer@gmail.com>
2017-01-21 15:33:15 -0500

    help

--- js/level.js
+++ js/level.js
@@ -41,7 +41,7 @@ function level(level_number){
 
     if(any_colission && ! done){
         done = true;
-        setTimeout(() => {console.log("hi"); window.game.stop(init)}, 3000);
+        setTimeout(() => {console.log("hi"); window.game.stop(init)}, 5000);
         console.log("asdf");
     }
   }
