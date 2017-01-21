40abb623942560dd9a47c947f4f39663af9beb77
Hastings Greer <hastings.greer@gmail.com>
2017-01-20 20:58:33 -0500

    wave

--- js/waver.js
+++ js/waver.js
@@ -1,20 +1,25 @@
 
-var waver = function(onStartWave, onEndWave, ctx){
+function waver (onStartWave, onEndWave, ctx){
   console.log("waver getting initialized");
-  var mouseX = 0;
-  var mouseY = 0;
-  var waving = false;
+  this.mouseX = 0;
+  this.mouseY = 0;
+  this.newX = 0;
+  this.newY = 0;
+  this.waving = false;
 
-  this.update = function(){
+  this.update = () => {
       //get mousex, y
-      if(mouseX != newX || mouseY != newY ){
-         if(!waving){
-             waving = true;
+      if(this.mouseX != this.newX || this.mouseY != this.newY ){
+          console.log(this.mouseX);
+          this.mouseX = this.newX;
+          this.mouseY = this.newY;
+         if(!this.waving){
+             this.waving = true;
              onStartWave();
          }
      } else {
-         if(waving){
-             waving = false;
+         if(this.waving){
+             this.waving = false;
              onEndWave();
          }
      }
@@ -27,30 +32,27 @@ var waver = function(onStartWave, onEndWave, ctx){
   var shoulderY = 500;
 
   //stolen from http://www.html5canvastutorials.com/advanced/html5-canvas-mouse-coordinates/
-  function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
-    };
+  this.getMousePos = (evt) => {
+
+      this.newX = evt.clientX - this.canvX;
+      this.newY = evt.clientY - this.canvY;
+
   }
   var canvas = document.getElementById('game');
 
   var rect = canvas.getBoundingClientRect();
-  var x = rect.left;
-  var y = rect.top;
+  this.canvX = rect.left;
+  this.canvY = rect.top;
 
-  canvas.addEventListener('mousemove', function(evt) {
-    var mousePos = getMousePos(canvas, evt);
-  }, false);
+  canvas.addEventListener('mousemove', this.getMousePos , false);
 
 
-  this.draw = function(){
+  this.draw = () => {
      ctx.save();
      ctx.translate(shoulderX,shoulderY);
-     ctx.rotate(Math.atan2(mouseY - shoulderY, mouseX -shoulderX));
-     ctx.translate(15, 8);
-     ctx.drawImage(hand);
+     ctx.rotate(Math.atan2(this.mouseX -shoulderX, -(this.mouseY - shoulderY)));
+     ctx.translate(-15, -30);
+     ctx.drawImage(hand, 0, 0);
      ctx.restore();
   }
   console.log("finished")
