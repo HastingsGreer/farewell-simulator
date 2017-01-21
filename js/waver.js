44a60cd2fdc729301ba41707a18ab08fa612ef37
Hastings Greer <hastings.greer@gmail.com>
2017-01-20 20:17:02 -0500

    hand

--- js/waver.js
+++ js/waver.js
@@ -1,17 +1,56 @@
 
 function waver(onStartWave, onEndWave, ctx){
-      mouseX = 0;
-      mouseY = 0;
-      waving = false;
+      var mouseX = 0;
+      var mouseY = 0;
+      var waving = false;
+
       this.update = function(){
           //get mousex, y
           if(mouseX != newX || mouseY != newY ){
-
-          }
+             if(!waving){
+                 waving = true;
+                 onStartWave();
+             }
+         } else {
+             if(waving){
+                 waving = false;
+                 onEndWave();
+             }
+         }
 
       }
-      this.draw = function(){
+      var hand = new Image();
+      hand.src = '/img/hand.png';
 
+      var shoulderX = 500;
+      var shoulderY = 500;
+
+      //stolen from http://www.html5canvastutorials.com/advanced/html5-canvas-mouse-coordinates/
+      function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
+      }
+      var canvas = document.getElementById('game');
+
+      var rect = canvas.getBoundingClientRect();
+      var x = rect.left;
+      var y = rect.top;
+
+      canvas.addEventListener('mousemove', function(evt) {
+        var mousePos = getMousePos(canvas, evt);
+      }, false);
+
+
+      this.draw = function(){
+         ctx.save();
+         ctx.translate(shoulderX,shoulderY);
+         ctx.rotate(Math.atan2(mouseY - shoulderY, mouseX -shoulderX));
+         ctx.translate(15, 8);
+         ctx.drawImage(hand);
+         ctx.restore();
       }
 
 }
