0d158afb59199b35da6a40a76c91d8b2faff4d1a
Hastings Greer <hastings.greer@gmail.com>
2017-01-21 13:17:10 -0500

    levels

--- js/init.js
+++ js/init.js
@@ -1,8 +1,42 @@
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
 function level_init(levelID) {
-    var cars = [new car(0, 100, 400, 0),
-                 new car(1, 100, -400, 1)];
-    var guests = [new guest(0, 0),
-                 new guest(100, 1)];
+    if( getQueryVariable("level")){
+        levelID = Number(getQueryVariable("level"));
+    }
+    var cars;
+    var guests;
+    switch (levelID) {
+        case 0:
+            cars = [new car(0, 100, 400, 0),
+                    new car(2, -800, -400, 0)];
+
+            guests = [new guest(200, 0)];
+            break;
+        case 1:
+            cars = [new car(0, 100, 400, 0),
+                    new car(1, 100, -400, 1),
+                    new car(2, -400, -400, 0),
+                    new car(3, -700, 300, 0)];
+
+            guests = [new guest(100, 0),
+                     new guest(200, 1)];
+            break;
+
+        default:
+            console.log("PANIC, why did you pass", levelID, "as level ID?");
+
+    }
+
 
     return {
         cars: cars,
