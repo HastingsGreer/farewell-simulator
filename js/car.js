559033d2c44953c2a77843a6e0a7bb4363b06dc0
Brandon Davis <bdavis@redspin.net>
2017-01-20 20:29:27 -0500

    Some skeletons for car, guest

---
+++ js/car.js
@@ -0,0 +1,10 @@
+
+function car(lane, position, speed){
+  this.lane = lane;
+  this.position = position;
+  this.speed = speed;
+
+  this.update = (delta) => {
+    // update the car position
+  }
+}
--- js/level.js
+++ js/level.js
@@ -1,4 +1,23 @@
 
-function level(){
-  
+function level(level_number){
+
+  /* 
+    Initialize the level.
+    This involves deciding positions of things
+    This involves drawing the background.
+  */
+  this.cars = [];
+  this.guests = [];
+
+  this.update = () => {
+    /* 
+      Run whatever update code is needed for the level
+    */
+  }
+
+  this.draw = (canvas) => {
+    /*
+      Let the level draw itself.
+    */
+  }
 }
