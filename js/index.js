00de8d9e8254d99077411ff25108eac29471f530
Brandon Davis <bdavis@redspin.net>
2017-01-20 19:32:03 -0500

    js template

---
+++ js/index.js
@@ -0,0 +1,25 @@
+function init(){
+  /*
+    Called by index.html on page load.
+    This is the entry point for this code.
+  */
+}
+
+
+function game(){
+  
+  this.update = function(){
+    /*
+      Called 30 times per second.  
+      Handle the game update logic here.
+      Don't update the canvas, just the internal state.
+    */
+  }
+
+  this.draw = function (){
+    /*
+      After the updates have been written to the game state, 
+      read the state and decide what changes to make to the canvas.
+    */
+  }
+}
