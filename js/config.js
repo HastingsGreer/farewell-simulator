127cc71eea9a91d96d560b8f00063870f5d63d33
Hastings Greer <hastings.greer@gmail.com>
2017-01-21 22:20:25 -0500

    alien

--- js/config.js
+++ js/config.js
@@ -22,9 +22,12 @@ var sb_position = {
 // Sprites - car
 var car_sprites = ["img/tank-roll.png", "img/car1-roll.png", "img/canoe-bear.png"];
 // sprites - guests
-var guestFronts = ["img/pirate-front-walk.png", "img/general-front-walk.png", "img/lady-front-walk.png"];
-var guestBacks = ["img/pirate-back-walk.png", "img/general-back-walk.png","img/lady-back-walk.png"];
-var guestWaves = ["img/pirate-dance-wave.png", "img/general-jump-wave.png", "img/lady-wave.png"];
+var guestFronts = ["img/pirate-front-walk.png", "img/general-front-walk.png",
+                   "img/lady-front-walk.png", "img/alien-front-walk.png"];
+var guestBacks = ["img/pirate-back-walk.png", "img/general-back-walk.png",
+                  "img/lady-back-walk.png", "img/alien-back-walk.png"];
+var guestWaves = ["img/pirate-dance-wave.png", "img/general-jump-wave.png",
+                  "img/lady-wave.png", "img/alien-wave.png"];
 
 // helper functions
 function rand_sign() {
